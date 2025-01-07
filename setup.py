23268da2788aaca2e1243ec9a7046c037ab82917
RohanBhattaraiNP <152933030+RohanBhattaraiNP@users.noreply.github.com>
2025-01-06 20:54:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 # Note: To use the 'upload' functionality of this file, you must:
 #   $ pip install twine
+
 import io, json
 import os
 import sys
 import glob
 from shutil import rmtree
+
 from setuptools import find_packages, setup, Command
+
+
 def read(fname):
     with open(fname, mode="r", encoding="utf-8") as f:
         src = f.read()
     return src
+
+
 def package_files(package, directory):
     os.chdir(package)
     paths = glob.glob(directory + "/**", recursive=True)
     os.chdir("..")
     return paths
+
+
 codemeta_json = "codemeta.json"
+
 # Let's pickup as much metadata as we need from codemeta.json
 with open(codemeta_json, mode="r", encoding="utf-8") as f:
     src = f.read()
     meta = json.loads(src)
+
 # Let's make our symvar string
 version = meta["version"]
+
 # Now we need to pull and format our author, author_email strings.
 author = ""
 author_email = ""
@@ -44,7 +56,9 @@ url = meta["codeRepository"]
 download = meta["downloadUrl"]
 license = meta["license"]
 name = meta["name"]
+
 REQUIRES_PYTHON = ">=3.6.0"
+
 # What packages are required for this module to be executed?
 REQUIRED = [
     "requests",
@@ -55,17 +69,22 @@ REQUIRED = [
     "cryptography",
     "s3cmd",
 ]
+
 # What packages are optional?
 EXTRAS = {
     # 'fancy feature': ['django'],
 }
+
 files = package_files("caltechdata_api", "vocabularies")
 files.append("vocabularies.yaml")
+
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
 # Except, perhaps the License and Trove Classifiers!
 # If you do change the License, remember to change the Trove Classifier for that!
+
 here = os.path.abspath(os.path.dirname(__file__))
+
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
@@ -73,6 +92,7 @@ try:
         long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = description
+
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not version:
@@ -80,29 +100,41 @@ if not version:
         exec(f.read(), about)
 else:
     about["__version__"] = version
+
+
 class UploadCommand(Command):
     """Support setup.py upload."""
+
     description = "Build and publish the package."
     user_options = []
+
     @staticmethod
     def status(s):
         """Prints things in bold."""
         print("\033[1m{0}\033[0m".format(s))
+
     def initialize_options(self):
         pass
+
     def finalize_options(self):
         pass
+
     def run(self):
         try:
             self.status("Removing previous builds…")
             rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
+
         self.status("Building Source and Wheel distribution…")
         os.system("{0} setup.py sdist bdist_wheel ".format(sys.executable))
+
         self.status("Uploading the package to PyPI via Twine…")
         os.system("twine upload dist/*")
+
         sys.exit()
+
+
 # Where the magic happens:
 setup(
     name=name,
